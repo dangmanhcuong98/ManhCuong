c0522dec1fafeca43b78b7d7a27decd14caddf1c
ManhCuong <ManhCuong@DESKTOP-SHRBOSO>
2019-04-08 19:23:41 +0700

    Add project files.

---
+++ BigSchool/Migrations/201904011358503_AddIsCanceledColumnToCourse.cs
@@ -0,0 +1,32 @@
+namespace BigSchool.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddIsCanceledColumnToCourse : DbMigration
+    {
+        public override void Up()
+        {
+            DropForeignKey("dbo.Courses", "Lecturer_Id", "dbo.AspNetUsers");
+            DropIndex("dbo.Courses", new[] { "Lecturer_Id" });
+            RenameColumn(table: "dbo.Courses", name: "Lecturer_Id", newName: "LecturerId");
+            AddColumn("dbo.Courses", "IsCanceled", c => c.Boolean(nullable: false));
+            AlterColumn("dbo.Courses", "LecturerId", c => c.String(nullable: false, maxLength: 128));
+            CreateIndex("dbo.Courses", "LecturerId");
+            AddForeignKey("dbo.Courses", "LecturerId", "dbo.AspNetUsers", "Id", cascadeDelete: true);
+            DropColumn("dbo.Courses", "LecturedId");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.Courses", "LecturedId", c => c.String(nullable: false));
+            DropForeignKey("dbo.Courses", "LecturerId", "dbo.AspNetUsers");
+            DropIndex("dbo.Courses", new[] { "LecturerId" });
+            AlterColumn("dbo.Courses", "LecturerId", c => c.String(maxLength: 128));
+            DropColumn("dbo.Courses", "IsCanceled");
+            RenameColumn(table: "dbo.Courses", name: "LecturerId", newName: "Lecturer_Id");
+            CreateIndex("dbo.Courses", "Lecturer_Id");
+            AddForeignKey("dbo.Courses", "Lecturer_Id", "dbo.AspNetUsers", "Id");
+        }
+    }
+}
